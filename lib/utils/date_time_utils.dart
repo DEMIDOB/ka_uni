9df7efacc48b80b730395141564fd19633702adb
Dan Demidov <dania.demidov@yandex.ru>
2025-11-02 20:16:28 +0100

    date time utils

---
+++ lib/utils/date_time_utils.dart
@@ -0,0 +1,10 @@
+String formatHumanReadableTimestamp(DateTime timestamp) {
+  final local = timestamp.toLocal();
+  final day = local.day.toString().padLeft(2, '0');
+  final month = local.month.toString().padLeft(2, '0');
+  final year = local.year.toString();
+  final hour = local.hour.toString().padLeft(2, '0');
+  final minute = local.minute.toString().padLeft(2, '0');
+
+  return "$day.$month.$year $hour:$minute";
+}
